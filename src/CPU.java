fe76387a39732aefdc62500ccfe839211d450882
Kunal Rana <kunalranarj2005@gmail.com>
2025-03-11 23:09:12 +0530

    Memory and CPU class work

--- src/CPU.java
+++ src/CPU.java
@@ -1,2 +1,9 @@
 public class CPU {
+
+    private Memory memory;
+    private Register[] registers;
+
+    public CPU(Memory memory) {
+        
+    }
 }
--- src/Memory.java
+++ src/Memory.java
@@ -1,4 +1,8 @@
 public class Memory {
+    byte[] memory;
 
-
+    public Memory(int memorySize) {
+        int cellCount = memorySize / 8;
+        memory = new byte[cellCount];
+    }
 }
