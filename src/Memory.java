d3d6eeeb9a9df8a00f68d5494817e0a308fe79f4
TheVansh2903 <shvansh2903@gmail.com>
2025-03-12 21:29:00 +0530

    implimented basic memory

--- src/Memory.java
+++ src/Memory.java
@@ -4,5 +4,16 @@ public class Memory {
     public Memory(int memorySize) {
         int cellCount = memorySize / 8;
         memory = new byte[cellCount];
+
     }
+    public byte read(int address) {
+        return memory[address];
+
+    }
+
+    public void write(int address, byte value) {
+        memory[address]=value;
+
+    }
+
 }
